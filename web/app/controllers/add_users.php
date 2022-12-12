cca93aa6dd1803460f33aab90a45812ed00485b5
tangzhongsi <tangzhongsi@qq.com>
2022-12-12 17:15:38 +0800

    update add_users.php

--- web/app/controllers/add_users.php
+++ web/app/controllers/add_users.php
@@ -8,24 +8,25 @@ if (!isset($_POST['password'])) {
     return;
 }
 
+$usergroup = $_POST['usergroup'] ? $_POST['usergroup'] : 'U';
 $username = $_POST['username'];
 $password = md5($_POST['password'] . getPasswordClientSalt());
 $motto = $_POST['motto'];
 $email = $username.'@zju.edu.cn';
 if (!validateUsername($username)) {
-    echo "失败：无效学号。";
+    echo $username . " 注册失败：无效学号。";
     return;
 }
 if (queryUser($username)) {
-    echo "失败：学号已存在。";
+    echo $username . " 注册失败：学号已存在。";
     return;
 }
 if (!validatePassword($password)) {
-    echo "失败：无效密码。";
+    echo $username .  "注册失败：无效密码。";
     return;
 }
 if (!validateEmail($email)) {
-    echo "失败：无效电子邮箱。";
+    echo $username . " 注册失败：无效电子邮箱。";
     return;
 }
 
@@ -35,7 +36,7 @@ $esc_email = DB::escape($email);
 
 $svn_pw = uojRandString(10);
 
-DB::query("insert into user_info (username, email, password, svn_password, register_time, motto) values ('$username', '$esc_email', '$password', '$svn_pw', '', '$motto')");
+DB::query("insert into user_info (usergroup, username, email, password, svn_password, register_time, motto) values ('$usergroup', '$username', '$esc_email', '$password', '$svn_pw', '', '$motto')");
 
 echo "欢迎你！" . $username . "，你已成功注册。";
 return;
